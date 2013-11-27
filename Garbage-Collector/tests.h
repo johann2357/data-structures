4d5b3a843ae7b5a12b57415ca73ea42376c8259a
johann2357 <johann2357@gmail.com>
2013-11-26 22:39:44 -0500

    more tests

--- Garbage-Collector/tests.h
+++ Garbage-Collector/tests.h
@@ -1,27 +1,51 @@
 #include "garbage_collector.h"
 #include "debug.h"
+#include <assert.h>
 
 struct UnitTest
 {
+  void status()
+  {
+    Debug("contains: ");
+    for (auto& a: GCTable)
+      Debug("  " << a.first << " : " << a.second);
+    Debug("\n");
+  }
   void test_pointer()
   {
-    SmartPtr<int> test = NULL;
-    test = new int(6);
+    Debug("test_pointer\n");
+    SmartPtr<int> test = new int(6);
     (*test) = 66;
-    Debug((*test) << '\n');
+    assert((*test) == 66);
+    /**/ status(); /**/
+    SmartPtr<int> copy = test;
+    assert((*copy) == 66);
+    /**/ status(); /**/ 
   }
   void test_array()
   {
+    Debug("test_array\n");
     SmartArray<int> test = NULL;
     test = new int [6];
-    test[0] = 66;
+    test[1] = 66;
     test[4] = 6;
-    Debug(test[0] << '\n');
-    Debug(test[4] << '\n');
+    assert(test[1] == 66);
+    assert(test[4] == 6);
+    /**/ status(); /**/
+    SmartArray<int> copy = test;
+    /**/ status(); /**/
+    test = new int [3];
+    test[1] = 666;
+    assert(test[1] == 666);
+    assert(copy[1] == 66);
+    /**/ status(); /**/
   }
   void run()
   {
+    /**/ status(); /**/ 
     test_pointer();
+    /**/ status(); /**/ 
     test_array();
+    /**/ status(); /**/ 
   }
 };
